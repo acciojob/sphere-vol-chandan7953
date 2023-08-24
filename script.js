cb5c7a14a1c75f3f946f89ef3c12a1b14a5e3029
Chandan Singh <74015019+chandan7953@users.noreply.github.com>
2023-08-24 12:56:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function volume_sphere() {
     //Write your code here
   // 4/3 π r³
 	let r = document.getElementById('radius').value;
-	let volume = (4*3.14*r*r*r);
+	let volume = (4*3.14*r*r*r)/3;
 	return volume;
 } 
 
